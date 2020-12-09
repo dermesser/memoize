d55dec4cffde75af6c65fbc83fff8c93b49e7470
Илья Ефимов <inferrna@gmail.com>
2020-12-10 01:06:52 +0800

    More complete test1

--- examples/test1.rs
+++ examples/test1.rs
@@ -19,11 +19,16 @@ fn hello(key: String) -> ComplexStruct {
     }
 }
 
+
+
 fn main() {
     println!("result: {:?}", hello("ABC".to_string()));
     println!("result: {:?}", hello("DEF".to_string()));
     println!("result: {:?}", hello("ABC".to_string()));  //Same as first
     thread::sleep(Duration::from_millis(2100));
+    println!("result: {:?}", hello("EFG".to_string()));
     println!("result: {:?}", hello("ABC".to_string()));  //Refreshed
+    println!("result: {:?}", hello("EFG".to_string()));  //Same as first
+    println!("result: {:?}", hello("ABC".to_string()));  //Same as refreshed
     println!("result: {:?}", memoized_original_hello("ABC".to_string()));
 }

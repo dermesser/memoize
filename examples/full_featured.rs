0f39d6bb3aa90dce4a80702e41a9a45238211084
Lewin Bormann <lbo@spheniscida.de>
2022-04-02 13:49:42 -0700

    Disable full-featured example if feature full not enabled
    for automated testing

--- examples/full_featured.rs
+++ examples/full_featured.rs
@@ -1,6 +1,6 @@
 use memoize::memoize;
-use std::time::{Instant, Duration};
 use std::thread;
+use std::time::{Duration, Instant};
 
 #[derive(Debug, Clone)]
 struct ComplexStruct {
@@ -9,6 +9,7 @@ struct ComplexStruct {
     i: Instant,
 }
 
+#[cfg(feature = "full")]
 #[memoize(TimeToLive: Duration::from_secs(2), Capacity: 123)]
 fn hello(key: String) -> ComplexStruct {
     println!("hello: {}", key);
@@ -20,13 +21,16 @@ fn hello(key: String) -> ComplexStruct {
 }
 
 fn main() {
-    println!("result: {:?}", hello("ABC".to_string()));
-    println!("result: {:?}", hello("DEF".to_string()));
-    println!("result: {:?}", hello("ABC".to_string()));  //Same as first
-    thread::sleep(core::time::Duration::from_millis(2100));
-    println!("result: {:?}", hello("EFG".to_string()));
-    println!("result: {:?}", hello("ABC".to_string()));  //Refreshed
-    println!("result: {:?}", hello("EFG".to_string()));  //Same as first
-    println!("result: {:?}", hello("ABC".to_string()));  //Same as refreshed
-    println!("result: {:?}", memoized_original_hello("ABC".to_string()));
+    #[cfg(feature = "full")]
+    {
+        println!("result: {:?}", hello("ABC".to_string()));
+        println!("result: {:?}", hello("DEF".to_string()));
+        println!("result: {:?}", hello("ABC".to_string())); //Same as first
+        thread::sleep(core::time::Duration::from_millis(2100));
+        println!("result: {:?}", hello("EFG".to_string()));
+        println!("result: {:?}", hello("ABC".to_string())); //Refreshed
+        println!("result: {:?}", hello("EFG".to_string())); //Same as first
+        println!("result: {:?}", hello("ABC".to_string())); //Same as refreshed
+        println!("result: {:?}", memoized_original_hello("ABC".to_string()));
+    }
 }

2d43d4834d04e5bd81a94d20559e92df45f0691d
Risto Saarelma <risto.saarelma@iki.fi>
2023-08-04 13:06:15 +0300

    Add an example that uses SharedCache
    This will catch the compile error on `cargo test`.

---
+++ examples/shared.rs
@@ -0,0 +1,16 @@
+use memoize::memoize;
+
+#[memoize(SharedCache)]
+fn hello(arg: String, arg2: usize) -> bool {
+    println!("{} => {}", arg, arg2);
+    arg.len() % 2 == arg2
+}
+
+fn main() {
+    // `hello` is only called once here.
+    assert!(!hello("World".to_string(), 0));
+    assert!(!hello("World".to_string(), 0));
+    // Sometimes one might need the original function.
+    assert!(!memoized_original_hello("World".to_string(), 0));
+    memoized_flush_hello();
+}

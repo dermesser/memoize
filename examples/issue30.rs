092f222d19fa611b73b5447cfbb568a88c6fbee0
Lewin Bormann <lewin@lewin-bormann.info>
2023-12-24 15:11:37 +0100

    Fix broken pipeline for --no-default-features in examples/issue30

--- examples/issue30.rs
+++ examples/issue30.rs
@@ -6,13 +6,18 @@ struct Struct {}
 #[derive(Clone)]
 struct Error {}
 
+#[cfg(feature = "full")]
 #[memoize(SharedCache, Capacity: 1024)]
 fn my_function(arg: &'static str) -> Result<Struct, Error> {
     println!("{}", arg);
     Ok(Struct{})
 }
 
+#[cfg(feature = "full")]
 fn main() {
     let s = "Hello World";
     my_function(s).ok();
 }
+
+#[cfg(not(feature = "full"))]
+fn main() {}
